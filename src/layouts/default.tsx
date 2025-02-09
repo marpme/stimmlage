7a01efc297bc1bd91db8c2bc9da1b371631bdcf2
marpme <7430964+marpme@users.noreply.github.com>
2025-02-09 15:10:27 +0100

    chore: update recognitions

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -7,22 +7,42 @@ export default function DefaultLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const currentYear = new Date().getFullYear();
+  const establishedYear = 2025;
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
       </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://heroui.com"
-          title="heroui.com homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">HeroUI</p>
-        </Link>
+      <footer className="w-full flex flex-col items-center justify-center py-3">
+        <div className="text-default-600">
+          Made with ❤️ in Germany &mdash; ©{" "}
+          {establishedYear - currentYear === 0
+            ? establishedYear
+            : `${establishedYear} - ${currentYear}`}
+        </div>
+        <div className="w-full flex items-center justify-center gap-1 py-3">
+          Daten von
+          <Link
+            isExternal
+            className="flex items-center gap-1"
+            href="https://dawum.de/"
+            title="Dawum - Neueste Wahlumfragen und Ergebnisse"
+          >
+            dawum.de
+          </Link>
+          licensed under
+          <Link
+            isExternal
+            className="flex items-center gap-1 "
+            href="https://opendatacommons.org/licenses/odbl/1-0/"
+            title="Open Database License (ODbL) v1.0"
+          >
+            Open Database License (ODbL)
+          </Link>
+        </div>
+        <p className="text-primary">HeroUI</p>
       </footer>
     </div>
   );
